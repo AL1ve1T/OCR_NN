e8dd84682be29b2143e92c80269aa1a6de74918f
aliveit.elnur@gmail.com <aliveit.elnur@gmail.com>
2020-03-22 13:14:05 +0400

    Sample TF code

--- run.py
+++ run.py
@@ -8,19 +8,14 @@ import tensorflow as tf
 import numpy as np
 
 image_size = 28
-train_images = 5
-test_images = 5
+train_images = 60000
+test_images = 10000
 
 train_images_path = 'dtset/train-images-idx3-ubyte.gz'
-train_labels_path = ''
-test_images_path = ''
-test_labels_path = ''
-
-buf = f.read(image_size * image_size * num_images)
-data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-data = data.reshape(num_images, image_size, image_size, 1)
-
-print(len(data[4][0]))
+train_labels_path = 'dtset/train-labels-idx1-ubyte.gz'
+test_images_path = 'dtset/t10k-images-idx3-ubyte.gz'
+test_labels_path = 'dtset/t10k-labels-idx1-ubyte.gz'
+checkpoint_path = 'checkpoints/model.ckpt'
 
 """
 import matplotlib.pyplot as plt
@@ -31,7 +26,7 @@ plt.show()
 
 ##
 
-def load_dataset(datapath, labelpath):
+def load_dataset(datapath, labelpath, size):
     f_data = gzip.open(datapath, 'r')
     f_labels = gzip.open(labelpath, 'r')
 
@@ -40,27 +35,40 @@ def load_dataset(datapath, labelpath):
     f_labels.read(8)
 
     # images
-    buf = f_data.read(image_size * image_size * num_images)
+    buf = f_data.read(image_size * image_size * size)
     data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-    data = data.reshape(num_images, image_size, image_size, 1)
+    data = data.reshape(size, image_size, image_size)
 
     # labels
-    buf = f_labels.read(image_size * image_size * num_images)
+    buf = f_labels.read(image_size * image_size * size)
     labels = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-    labels = labels.reshape(num_images, image_size, image_size, 1)
+
+    data = data / 255.0
 
     return data, labels
 
-def build_graph():
-    train_imgs, train_labels = load_dataset(train_images_path, train_labels_path)
-    test_imgs, test_labels = load_dataset(test_images_path, test_labels_path)
+def train():
+    train_imgs, train_labels = load_dataset(train_images_path, train_labels_path, train_images)
+    test_imgs, test_labels = load_dataset(test_images_path, test_labels_path, test_images)
 
-    
+    # Step 1: Define model
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10)
+    ])
+
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    model.compile(optimizer='adam',
+                  loss=loss_fn,
+                  metrics=['accuracy'])
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+    model.fit(train_imgs, train_labels, epochs=20, callbacks=[cp_callback])
     
 
 if __name__ == '__main__':
-    load_dataset()
-    build_graph()
-      train()
-      test()
-    extract()
+    train()
+    # extract()
